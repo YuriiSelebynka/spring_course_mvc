f04b74c97a735a65752598204356470ccd0cca6a
YuriiSelebynka <selebinkayuriy@gmail.com>
2021-06-19 20:46:42 +0300

    for education

--- src/main/java/com/yuriiselebynka/spring/mvc/Employee.java
+++ src/main/java/com/yuriiselebynka/spring/mvc/Employee.java
@@ -1,11 +1,19 @@
 package com.yuriiselebynka.spring.mvc;
 
+import com.yuriiselebynka.spring.mvc.validation.CheckEmail;
+
+import javax.validation.constraints.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Employee {
+    @Size(min = 2, message = "name must be minimum 2 symbols")
     private String name;
+//    @NotEmpty(message = "surname is required field")
+    @NotBlank(message = "surname is required field")
     private String surname;
+    @Min(value = 500, message = "must be greater than 499")
+    @Max(value = 1000, message = "must be less than 1001")
     private int salary;
     private String department;
     private Map<String, String> departments;
@@ -13,6 +21,11 @@ public class Employee {
     private Map<String, String> carBrands;
     private String[] languages;
     private Map<String, String> languageList;
+    @CheckEmail(value = "abc.com", message = "email must ends with abc.com")
+    private String email;
+
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{2}", message = "please use pattern XXX-XX-XX")
+    private String phoneNumber;
 
     public Employee() {
         departments = new HashMap<>();
@@ -101,6 +114,22 @@ public class Employee {
 
     public void setLanguageList(Map<String, String> languageList) {
         this.languageList = languageList;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
--- src/main/java/com/yuriiselebynka/spring/mvc/MyController.java
+++ src/main/java/com/yuriiselebynka/spring/mvc/MyController.java
@@ -2,11 +2,13 @@ package com.yuriiselebynka.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -32,24 +34,36 @@ public class MyController {
 
     @RequestMapping("/showDetails")
     public String showEmpDetails(
+            @Valid @ModelAttribute("employee") Employee emp
+            , BindingResult bindingResult) {
+
+//        System.out.println("surname length = " + emp.getSurname().length());
+
+            if (bindingResult.hasErrors()) {
+                return "ask-emp-details-view";
+            }
+            else {
+                return "show-emp-details-view";
+            }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(
 //            @RequestParam("employeeName") String empName
-//            , Model model
-            @ModelAttribute("employee") Employee emp
-    ) {
+//            , Model model) {
 
-        String name = emp.getName();
-        emp.setName("Mr. " + name);
-
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "!");
-
-        int salary = emp.getSalary();
-        emp.setSalary(salary * 10);
+//        String name = emp.getName();
+//        emp.setName("Mr. " + name);
+//
+//        String surname = emp.getSurname();
+//        emp.setSurname(surname + "!");
+//
+//        int salary = emp.getSalary();
+//        emp.setSalary(salary * 10);
 
 //        empName = "Mr. " + empName + "!";
 //        model.addAttribute("nameAttribute", empName);
 
-        return "show-emp-details-view";
+
     }
 
 //    @RequestMapping("showDetails")
